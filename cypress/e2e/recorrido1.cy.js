bcfcb3100d3631c540ba66502753972cc7daf602
Lucas Magnani <lucas96aj@gmail.com>
2025-06-25 15:08:10 -0300

    Script test Registro y login

---
+++ cypress/e2e/recorrido1.cy.js
@@ -0,0 +1,29 @@
+describe('Recorrido 1', () => {
+  it('Un usuario se registra, se loguea y ve los libros disponibles', () => {
+    cy.visit('http://localhost:5173/');
+    cy.get('ul').contains('Registrar').click();
+    cy.location('pathname').should('equal', '/register');
+
+    cy.get('input').eq(0).type('Fernando');
+    cy.get('input').eq(1).type('fernando@gmail.com');
+    cy.get('input').eq(2).type('Fernando123');
+    cy.get('button').contains('Registrar').click();
+    cy.get('.chakra-alert__title')
+      .should('exist')
+      .contains('Ya existe un usuario con el email: fernando@gmail.com');
+    
+    cy.get('ul svg').click();
+    cy.location('pathname').should('equal', '/login');
+    
+    cy.get('input').eq(0).type('fernando@gmail.com');
+    cy.get('input').eq(1).type('Fernando123');
+    cy.get('button').contains('Iniciar Sesion').click();
+    
+    cy.location('pathname').should('equal', '/perfil');
+    cy.get('h1').contains('hola Fernando');
+
+    cy.get('ul').contains('Libros').click();
+    cy.location('pathname').should('equal', '/books');
+    cy.get('.cards-grid').should('be.visible');
+  })
+})
